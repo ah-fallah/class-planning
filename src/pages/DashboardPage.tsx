13e2927599e0d0c1e76ec33f17f09c694a7b8282
AmirHossein Fallah <0amirex0@gmail.com>
2026-08-27 22:41:32 +0330

    Remove unused Input import

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -28,7 +28,6 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
-import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { NumberInput } from '@/components/NumberInput'
 import {
